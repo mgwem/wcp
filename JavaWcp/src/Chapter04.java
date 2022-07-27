594f19dcaf04a914993778bf7cabc64f6df05ccb
EmiriMaegawa <101238309+mgwem@users.noreply.github.com>
2022-07-27 16:40:37 +0900

    [Add]4章

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,12 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str1 = "Yamada Taro";
+		System.out.println(str1);
+		str1 = "です";
+		System.out.println(str1);
+		
+		int num = 1;
+		System.out.println(num);
+	}
+}
