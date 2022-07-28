f93c386333dd24cd90d74277707e86c12b0daa13
EmiriMaegawa <101238309+mgwem@users.noreply.github.com>
2022-07-28 09:52:03 +0900

    [Add]5章

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,33 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 122L;
+		System.out.println(long1);
+		
+		double double1 = 0.5;
+		System.out.println(double1);
+		
+		boolean boolean1 = false;
+		System.out.println(boolean1);
+		
+		Float float1 = 0.12f;
+		System.out.println(float1);
+		
+		Integer integer1 = 34;
+		System.out.println(integer1);
+		
+		Character character1 = 'A';
+		System.out.println(character1);
+		
+		int[] array = new int[5];
+		array[0] = 1;
+		array[1] = 2;
+		array[2] = 3;
+		array[3] = 4;
+		array[4] = 5;
+		System.out.println(array[3]);
+		
+		int[] array1 = { 0, 10, 20, 30, 40 }; 
+		System.out.println(array1[3]);
+	}
+}
