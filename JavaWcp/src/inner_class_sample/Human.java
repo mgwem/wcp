ac1a0dce092a70c2d87235076026f783ec434444
EmiriMaegawa <101238309+mgwem@users.noreply.github.com>
2022-08-14 15:26:11 +0900

    [Add]1章(Human)

---
+++ JavaWcp/src/inner_class_sample/Human.java
@@ -0,0 +1,16 @@
+package inner_class_sample;
+
+public class Human {
+	public static void main(String[] args) {
+		Human human = new Human();
+		Human.HumanLeg leg = human.new HumanLeg();
+		leg.kick();
+	}
+	
+	public class HumanLeg {
+		public void kick() {
+			System.out.println("蹴る");
+		}
+		
+	}
+}
