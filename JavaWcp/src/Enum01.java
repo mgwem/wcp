f8768e32aff2386707f5264006461c12b3c95aa1
EmiriMaegawa <101238309+mgwem@users.noreply.github.com>
2022-08-16 16:38:57 +0900

    [Add]2章(Enum01)

---
+++ JavaWcp/src/Enum01.java
@@ -0,0 +1,8 @@
+
+public class Enum01 {
+	public static void main(String[] args) {
+		System.out.println(Dessert.APPLE);
+		System.out.println(Dessert.ICE_CREAM);
+		System.out.println(Dessert.CAKE);
+	}
+}
