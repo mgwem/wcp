a84f38ed1651485acf79afe8bd27d7483df65fb6
EmiriMaegawa <101238309+mgwem@users.noreply.github.com>
2022-08-17 09:21:15 +0900

    [Add]2章(Enum02)

---
+++ JavaWcp/src/Enum02.java
@@ -0,0 +1,12 @@
+
+public class Enum02 {
+	public static void main(String[] args) {
+		Dessert apple = Dessert.APPLE;
+		Dessert iceCream = Dessert.ICE_CREAM;
+		Dessert cake = Dessert.CAKE;
+		
+		System.out.println(apple);
+		System.out.println(iceCream);
+		System.out.println(cake);
+	}
+}
